a33bf06bd6f9318574b05ca6f3f0ee9ee327bf70
Seroph <ajfrank@igetno.com>
2026-04-10 19:45:37 -0500

    Add blood drop indicator for low-health creatures

--- src/functions.ts
+++ src/functions.ts
@@ -298,6 +298,20 @@ function getHpProgressClass(combatant: Pick<Combatant, "currentHP" | "totalHP">)
     }[getHpStatus(combatant)]
 }
 
+function getBloodDropCount(combatant: Pick<Combatant, "currentHP" | "totalHP">): number {
+    const hpRatio = getHpRatio(combatant)
+
+    if (hpRatio < 0.25) {
+        return 2
+    }
+
+    if (hpRatio < 0.5) {
+        return 1
+    }
+
+    return 0
+}
+
 function getVisibleCombatantAtOrAfter<T extends Pick<Combatant, "visibility">>(
     combatants: T[],
     startIndex: number
@@ -331,5 +345,5 @@ function getConditionTextClass(backgroundColor: string): string {
     return colorIsDark(backgroundColor) ? "text-white" : "text-black"
 }
 
-export {colorIsDark, combatantColorKeys, formatCombatantName, getConditionTextClass, getHpProgressClass, getHpRatio, getHpStatus, getHpTextClass, getVisibleCombatantAtOrAfter, Visibility, Condition, Combatant, defaultCombatants, getDefaultCombatants}
+export {colorIsDark, combatantColorKeys, formatCombatantName, getBloodDropCount, getConditionTextClass, getHpProgressClass, getHpRatio, getHpStatus, getHpTextClass, getVisibleCombatantAtOrAfter, Visibility, Condition, Combatant, defaultCombatants, getDefaultCombatants}
 export type { CombatantColorKey }
